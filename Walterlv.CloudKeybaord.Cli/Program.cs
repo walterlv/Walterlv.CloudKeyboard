8810e7354cc9192d39f1e3d908668cdee3c57c26
walterlv <xblero@gmail.com>
2019-01-19 17:31:52 +0800

    Add cli simulation.

---
+++ Walterlv.CloudKeybaord.Cli/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Walterlv.CloudTyping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = Process.GetProcesses().Select(x => x.MainWindowTitle)
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Where(x => x.StartsWith("Walterlv Cloud Keyboard")).ToArray();
+            if (strings.Length >= 1)
+            {
+                RunAsMobileEnd().Wait();
+            }
+            else
+            {
+                RunAsPcEnd().Wait();
+            }
+        }
+
+        private static async Task RunAsPcEnd()
+        {
+            Console.Title = "Walterlv Cloud Keyboard - PC";
+            var token = ReadSingleLineText("Input a token: ");
+            var keyboard = new CloudKeyboard(token);
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                await keyboard.PutText(line);
+            }
+        }
+
+        private static async Task RunAsMobileEnd()
+        {
+            Console.Title = "Walterlv Cloud Keyboard - Mobile";
+            var token = ReadSingleLineText("Input a token: ");
+            var keyboard = new CloudKeyboard(token);
+
+            while (true)
+            {
+                var text = (await keyboard.GetText()).Text;
+                Console.CursorTop = 1;
+                Console.CursorLeft = 0;
+                for (var i = 0; i < 320; i++)
+                {
+                    Console.Write(' ');
+                }
+
+                Console.CursorTop = 1;
+                Console.CursorLeft = 0;
+                Console.Write(text);
+                await Task.Delay(1000);
+            }
+        }
+
+        private static string ReadSingleLineText(string tip)
+        {
+            string result;
+
+            do
+            {
+                Console.Write(tip);
+                result = Console.ReadLine()?.Trim().ToLowerInvariant();
+            } while (string.IsNullOrWhiteSpace(result));
+
+            return result;
+        }
+    }
+}
