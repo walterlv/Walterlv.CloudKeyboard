a65052e1c8d20200266d4ddeb965989da7bdfbcc
walterlv <xblero@gmail.com>
2019-01-19 16:15:34 +0800

    Add core project.

---
+++ Walterlv.CloudKeyboard.Core/TypingText.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace Walterlv.CloudTyping
+{
+    public readonly struct TypingText
+    {
+        public TypingText(string text)
+        {
+            Text = text ?? "";
+            CaretStartIndex = Text.Length;
+            CaretEndIndex = CaretStartIndex;
+        }
+
+        public TypingText(string text, int caretIndex)
+        {
+            Text = text ?? "";
+            CaretStartIndex = caretIndex;
+            CaretEndIndex = caretIndex;
+        }
+
+        [JsonConstructor]
+        public TypingText(string text, int caretStartIndex, int caretEndIndex)
+        {
+            Text = text;
+            CaretStartIndex = caretStartIndex;
+            CaretEndIndex = caretEndIndex;
+        }
+
+        public string Text { get; }
+
+        public int CaretStartIndex { get; }
+
+        public int CaretEndIndex { get; }
+    }
+}
