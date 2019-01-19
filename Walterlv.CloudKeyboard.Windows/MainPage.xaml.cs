982f2a03d14dda665a499574a1ccf6ef9f658cba
walterlv <xblero@gmail.com>
2019-01-19 21:50:32 +0800

    Send text.

--- Walterlv.CloudKeyboard.Windows/MainPage.xaml.cs
+++ Walterlv.CloudKeyboard.Windows/MainPage.xaml.cs
@@ -1,12 +1,28 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Walterlv.CloudTyping
 {
     public sealed partial class MainPage : Page
     {
+        private CloudKeyboard _keyboard;
+
         public MainPage()
         {
             InitializeComponent();
+            _keyboard = new CloudKeyboard("0");
+        }
+
+        private async void TypingTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            await _keyboard.SetTextAsync(TypingTextBox.Text,
+                TypingTextBox.SelectionStart, TypingTextBox.SelectionStart + TypingTextBox.SelectionLength);
+        }
+
+        private async void TypingTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            await _keyboard.SetTextAsync(TypingTextBox.Text,
+                TypingTextBox.SelectionStart, TypingTextBox.SelectionStart + TypingTextBox.SelectionLength);
         }
     }
 }
