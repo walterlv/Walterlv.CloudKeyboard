973af3422a867e8c422e8ca7e95df21eec6bb8f5
walterlv <xblero@gmail.com>
2019-01-19 21:43:15 +0800

    Add universal.

---
+++ Walterlv.CloudKeyboard.Windows/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Walterlv.CloudTyping
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
