9798e5e05e01f06318bed3b85cd52d0a6075cfdf
walterlv <xblero@gmail.com>
2019-01-20 15:20:44 +0800

    Publish to external server.

--- Walterlv.CloudKeyboard.Core/HostInfo.cs
+++ Walterlv.CloudKeyboard.Core/HostInfo.cs
@@ -2,6 +2,6 @@
 {
     public static class HostInfo
     {
-        public const string BaseUrl = "http://172.18.88.107:5000/api/keyboard";
+        public const string BaseUrl = "http://172.96.16.169:5000/api/keyboard";
     }
 }
