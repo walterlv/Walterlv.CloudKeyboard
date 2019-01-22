d1211d3954ff390f0f6c479eecf3c95f09627171
walterlv <xblero@gmail.com>
2019-01-22 21:20:24 +0800

    Add empty text tips.

---
+++ CloudKeyboard.Wpf/Utils/EmptyStringToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Walterlv.CloudTyping.Utils
+{
+    public class EmptyStringToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public EmptyStringToVisibilityConverter() => IsVisibleWhenEmpty = false;
+
+        public EmptyStringToVisibilityConverter(bool isVisibleWhenEmpty) => IsVisibleWhenEmpty = isVisibleWhenEmpty;
+
+        [ConstructorArgument("isVisibleWhenEmpty")]
+        public bool IsVisibleWhenEmpty { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var whenEmpty = IsVisibleWhenEmpty ? Visibility.Visible : Visibility.Collapsed;
+            var whenNotEmpty = IsVisibleWhenEmpty ? Visibility.Collapsed : Visibility.Visible;
+            return string.IsNullOrEmpty(value as string)
+                ? whenEmpty
+                : whenNotEmpty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
