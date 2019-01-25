efbdcc068b71907c4239c11cc66d943753f9b6c4
walterlv <xblero@gmail.com>
2019-01-25 21:11:26 +0800

    Remove useless properties.

--- CloudKeyboard.WebApi/Models/KeyboardContext.cs
+++ CloudKeyboard.WebApi/Models/KeyboardContext.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using TypingTextCore = Walterlv.CloudTyping.TypingText;
-using TypingTextModel = Walterlv.CloudTyping.Models.TypingText;
-using TypingRepo = System.Collections.Concurrent.ConcurrentDictionary<string, System.Collections.Concurrent.ConcurrentQueue<Walterlv.CloudTyping.TypingText>>;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Walterlv.CloudTyping.Models
 {
@@ -15,11 +9,5 @@ namespace Walterlv.CloudTyping.Models
         }
 
         public DbSet<Keyboard> Keyboards { get; set; }
-        public DbSet<TypingTextModel> TypingTexts { get; set; }
-
-        public TypingRepo TypingTextRepo = new TypingRepo(new Dictionary<string, string>
-        {
-            {"0", "Welcome to use walterlv's cloud keyboard."},
-        }.ToDictionary(x => x.Key, x => new ConcurrentQueue<TypingTextCore>(new[] {new TypingTextCore(x.Value)})));
     }
 }
