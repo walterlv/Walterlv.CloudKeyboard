f64cb72944abfafb3216a4883f8307bbf3db493e
walterlv <xblero@gmail.com>
2019-01-25 20:32:42 +0800

    Use primary key.

--- CloudKeyboard.WebApi/Models/Keyboard.cs
+++ CloudKeyboard.WebApi/Models/Keyboard.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using TypingTextModel = Walterlv.CloudTyping.Models.TypingText;
 
 namespace Walterlv.CloudTyping.Models
 {
     public class Keyboard
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
         public string Token { get; set; }
 
         public virtual List<TypingTextModel> Typings { get; set; }
