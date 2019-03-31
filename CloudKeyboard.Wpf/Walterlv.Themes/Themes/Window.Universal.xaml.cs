3880ca04bfdd0cd99cf3969450ed662558bb4255
walterlv <xblero@gmail.com>
2019-03-31 19:39:39 +0800

    使用最新的 Fluent UI

--- CloudKeyboard.Wpf/Walterlv.Themes/Themes/Window.Universal.xaml.cs
+++ CloudKeyboard.Wpf/Walterlv.Themes/Themes/Window.Universal.xaml.cs
@@ -3,7 +3,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace Walterlv.CloudTyping.Themes
+// ReSharper disable CheckNamespace
+
+namespace Walterlv.Themes
 {
     public class UniversalWindowStyle
     {
@@ -16,6 +18,16 @@ namespace Walterlv.CloudTyping.Themes
 
         public static void SetTitleBar(DependencyObject element, UniversalTitleBar value)
             => element.SetValue(TitleBarProperty, value);
+
+        public static readonly DependencyProperty WindowProperty = DependencyProperty.RegisterAttached(
+            "Window", typeof(UniversalWindow), typeof(UniversalWindowStyle),
+            new PropertyMetadata(new UniversalWindow(), OnWindowChanged));
+
+        public static UniversalWindow GetWindow(DependencyObject element) =>
+            (UniversalWindow)element.GetValue(WindowProperty);
+
+        public static void SetWindow(DependencyObject element, UniversalWindow value) =>
+            element.SetValue(WindowProperty, value);
 
         public static readonly DependencyProperty TitleBarButtonStateProperty = DependencyProperty.RegisterAttached(
             "TitleBarButtonState", typeof(WindowState?), typeof(UniversalWindowStyle),
@@ -38,6 +50,11 @@ namespace Walterlv.CloudTyping.Themes
             => element.SetValue(IsTitleBarCloseButtonProperty, value);
 
         private static void OnTitleBarChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is null) throw new NotSupportedException("TitleBar property should not be null.");
+        }
+
+        private static void OnWindowChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (e.NewValue is null) throw new NotSupportedException("TitleBar property should not be null.");
         }
@@ -99,5 +116,19 @@ namespace Walterlv.CloudTyping.Themes
         public Color ButtonHoverBackground { get; set; } = Color.FromRgb(0xE6, 0xE6, 0xE6);
         public Color ButtonPressedForeground { get; set; } = Colors.Black;
         public Color ButtonPressedBackground { get; set; } = Color.FromRgb(0xCC, 0xCC, 0xCC);
+    }
+
+    public class UniversalWindow
+    {
+        public Color FrameColor { get; set; } = SystemParameters.WindowGlassColor;
+        public Color InactiveFrameColor { get; set; } = Colors.DimGray;
+    }
+
+    public class UniversalWindowParameters
+    {
+        public static double DefaultWindowWidth { get; } = (int)SystemParameters.PrimaryScreenHeight;
+        public static double DefaultWindowHeight { get; } = (int)(SystemParameters.PrimaryScreenHeight * 0.75);
+        public static double DefaultMinWindowWidth { get; } = 500;
+        public static double DefaultMinWindowHeight { get; } = 500;
     }
 }
