cf3c67a031d15154fb367e281891e57fb13e5fd5
walterlv <xblero@gmail.com>
2020-02-26 17:10:57 +0800

    更新调试地址

--- CloudKeyboard.Core/HostInfo.cs
+++ CloudKeyboard.Core/HostInfo.cs
@@ -2,6 +2,12 @@
 {
     public static class HostInfo
     {
+#if DEBUG
+        // DEBUG 下使用本地调试地址。
+        public const string BaseUrl = "http://localhost:5000/api/keyboard";
+#else
+        // RELEASE 下使用正式环境地址。
         public const string BaseUrl = "https://cloud-keyboard.walterlv.com:8000/api/keyboard";
+#endif
     }
 }
