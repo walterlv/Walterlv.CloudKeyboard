bcbae1a753ec7ce6d7a948b5fc8d839ce699e68a
lindexi <lindexi_gd@163.com>
2020-12-03 19:48:28 +0800

    修改访问链接

--- CloudKeyboard.Core/HostInfo.cs
+++ CloudKeyboard.Core/HostInfo.cs
@@ -4,10 +4,10 @@
     {
 #if DEBUG
         // DEBUG 下使用本地调试地址。
-        public const string BaseUrl = "http://localhost:5000/api/keyboard";
+        public const string BaseUrl = "https://cloud-keyboard.walterlv.com:4096/api/keyboard";
 #else
         // RELEASE 下使用正式环境地址。
-        public const string BaseUrl = "https://cloud-keyboard.walterlv.com:8000/api/keyboard";
+        public const string BaseUrl = "https://cloud-keyboard.walterlv.com:4096/api/keyboard";
 #endif
     }
 }
